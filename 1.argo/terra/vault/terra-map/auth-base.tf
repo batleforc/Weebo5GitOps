f2806ecd81df9ebf2c081bd3be9c563ecf241b13
Max Batleforc <maxleriche.60@gmail.com>
2026-04-07 22:13:57 +0200

    feat: change ns for vpn

--- 1.argo/terra/vault/terra-map/auth-base.tf
+++ 1.argo/terra/vault/terra-map/auth-base.tf
@@ -66,7 +66,7 @@ resource "vault_kubernetes_auth_backend_role" "auth-write" {
 resource "vault_kubernetes_auth_backend_role" "auth-read" {
   role_name                        = "auth-read"
   bound_service_account_names      = ["authentik", "default"]
-  bound_service_account_namespaces = ["auth", "argocd", "netbird", "che", "grafana", "harbor"]
+  bound_service_account_namespaces = ["auth", "argocd", "vpn", "che", "grafana", "harbor"]
   token_ttl                        = 3600
   token_policies                   = [vault_policy.mv_reader_policy.name]
 }
