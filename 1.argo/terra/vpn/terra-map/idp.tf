1617959c735f31cc00e1f12e1e3c88c4efcbd46a
Max Batleforc <maxleriche.60@gmail.com>
2026-04-09 22:34:27 +0200

    feat: wip

--- 1.argo/terra/vpn/terra-map/idp.tf
+++ 1.argo/terra/vpn/terra-map/idp.tf
@@ -5,7 +5,7 @@ data "vault_kv_secret_v2" "authentik" {
 
 resource "netbird_identity_provider" "authentik" {
   name          = "Batleforc SSO"
-  type          = "authentik"
+  type          = "oidc"
   client_id     = data.vault_kv_secret_v2.authentik.data["NETBIRD_AUTH_CLIENT_ID"]
   client_secret = data.vault_kv_secret_v2.authentik.data["NETBIRD_AUTH_CLIENT_SECRET"]
   issuer        = data.vault_kv_secret_v2.authentik.data["NETBIRD_AUTH_BASE_URL"]
