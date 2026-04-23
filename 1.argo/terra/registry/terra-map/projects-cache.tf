b5a1dc5ea246e57a81a230212110ce47c94eae15
Max Batleforc <maxleriche.60@gmail.com>
2026-04-23 23:21:13 +0200

    feat: make quay available

--- 1.argo/terra/registry/terra-map/projects-cache.tf
+++ 1.argo/terra/registry/terra-map/projects-cache.tf
@@ -36,12 +36,12 @@ resource "harbor_project" "cache-ghub" {
   storage_quota = 15
 }
 
-# resource "harbor_project" "cache-quay" {
-#   name          = "cache-quay"
-#   registry_id   = resource.harbor_registry.quay.registry_id
-#   public        = "false"
-#   storage_quota = 15
-# }
+resource "harbor_project" "cache-quay" {
+  name          = "cache-quay"
+  registry_id   = resource.harbor_registry.quay.registry_id
+  public        = "false"
+  storage_quota = 15
+}
 
 resource "harbor_project" "cache-talos" {
   name          = "cache-talos"
@@ -64,12 +64,12 @@ resource "harbor_project_member_group" "admin_cache_ghub" {
   type          = "oidc"
 }
 
-# resource "harbor_project_member_group" "admin_cache_quay" {
-#   project_id    = harbor_project.cache-quay.id
-#   group_name    = "weebo_admin"
-#   role          = "maintainer"
-#   type          = "oidc"
-# }
+resource "harbor_project_member_group" "admin_cache_quay" {
+  project_id    = harbor_project.cache-quay.id
+  group_name    = "weebo_admin"
+  role          = "maintainer"
+  type          = "oidc"
+}
 
 resource "harbor_project_member_group" "admin_cache_talos" {
   project_id    = harbor_project.cache-talos.id
