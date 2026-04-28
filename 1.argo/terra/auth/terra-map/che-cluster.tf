41cd32f192468e9e2348f719eb3914a11234e120
Max Batleforc <maxleriche.60@gmail.com>
2026-04-29 00:06:17 +0200

    feat: wip

---
+++ 1.argo/terra/auth/terra-map/che-cluster.tf
@@ -0,0 +1,47 @@
+resource "authentik_provider_oauth2" "che" {
+  name                  = "che-cluster"
+  client_id             = "che-cluster"
+  sub_mode              = "user_username"
+  invalidation_flow     = data.authentik_flow.default-invalidation-flow.id
+  authorization_flow    = data.authentik_flow.default-authorization-flow.id
+  signing_key           = data.authentik_certificate_key_pair.generated.id
+  access_token_validity = "hours=10"
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict",
+      url           = "http://localhost:8000",
+    },
+    {
+      matching_mode = "strict",
+      url           = "https://cde.batleforc.fr/oauth/callback",
+    },
+    {
+      matching_mode = "regex",
+      url           = "https://.*.cde.batleforc.fr/*.",
+    },
+  ]
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.scope-email.id,
+    data.authentik_property_mapping_provider_scope.scope-profile.id,
+    data.authentik_property_mapping_provider_scope.scope-openid.id,
+    data.authentik_property_mapping_provider_scope.scope-offline.id,
+  ]
+}
+
+resource "authentik_application" "che" {
+  name              = "che-cluster"
+  slug              = "che-cluster"
+  protocol_provider = authentik_provider_oauth2.che.id
+}
+
+resource "vault_kv_secret_v2" "che-app" {
+  mount = "mv"
+  name  = "eclipse-che/auth"
+  data_json = jsonencode(
+    {
+      AUTHENTIK_CLIENT_ID     = authentik_provider_oauth2.che.client_id,
+      AUTHENTIK_CLIENT_SECRET = authentik_provider_oauth2.che.client_secret,
+      AUTHENTIK_URL           = "https://login.4.weebo.fr/application/o/${authentik_application.che.slug}/",
+    }
+  )
+}
