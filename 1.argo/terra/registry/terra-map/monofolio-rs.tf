8edbfeadeb398b8ff523186237d2f4a4ac582e29
Max Batleforc <maxleriche.60@gmail.com>
2026-05-01 17:07:02 +0200

    feat: wip

--- 1.argo/terra/registry/terra-map/monofolio-rs.tf
+++ 1.argo/terra/registry/terra-map/monofolio-rs.tf
@@ -106,7 +106,7 @@ resource "vault_kv_secret_v2" "rw-monofolio-rs" {
     {
       username = "${harbor_config_system.main.robot_name_prefix}${harbor_robot_account.rw-monofolio-rs.name}"
       password = harbor_robot_account.rw-monofolio-rs.secret
-      url      = "https://registry.batleforc.fr"
+      url      = "registry.batleforc.fr"
     }
   )
 }
