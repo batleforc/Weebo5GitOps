d4f8446dfb93c24711a1651427f34ac32ebbd0ec
Max Batleforc <maxleriche.60@gmail.com>
2026-05-08 02:44:14 +0200

    feat: wip

--- 1.argo/terra/git/terra-map/forgejo.tf
+++ 1.argo/terra/git/terra-map/forgejo.tf
@@ -7,3 +7,9 @@ resource "forgejo_repository" "buildah" {
   name        = "buildah"
   description = "Repo for the buildah base image used in CI pipelines"
 }
+
+resource "forgejo_repository" "runner" {
+  owner       = forgejo_organization.forgejo.name
+  name        = "runner"
+  description = "Repo for the runner used in CI pipelines"
+}
