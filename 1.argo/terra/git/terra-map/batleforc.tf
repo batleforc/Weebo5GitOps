2881911166bedcca3ff07d70d12994488005c2ca
Max Batleforc <maxleriche.60@gmail.com>
2026-05-08 02:38:20 +0200

    feat: add monofolio-rs repo

--- 1.argo/terra/git/terra-map/batleforc.tf
+++ 1.argo/terra/git/terra-map/batleforc.tf
@@ -3,3 +3,11 @@ resource "forgejo_repository" "Weebo5GitOps" {
   description = "Repo for the Weebo5 GitOps"
   auto_init   = false
 }
+
+
+
+resource "forgejo_repository" "monofolio-rs" {
+  name        = "monofolio-rs"
+  description = "Portfolio website for batleforc.fr, built with Rust and Leptos"
+  auto_init   = false
+}
