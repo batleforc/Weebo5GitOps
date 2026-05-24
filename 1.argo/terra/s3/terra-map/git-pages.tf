a81ad30efac412928698777a131850c99644897d
Max Batleforc <maxleriche.60@gmail.com>
2026-05-24 22:59:22 +0200

    feat: wip

--- 1.argo/terra/s3/terra-map/git-pages.tf
+++ 1.argo/terra/s3/terra-map/git-pages.tf
@@ -12,29 +12,22 @@ resource "random_password" "git_pages_admin_id" {
   override_special = "_-"
 }
 
-resource "rustfs_serviceaccount" "git_pages" {
+resource "rustfs_policy" "git_pages" {
   name       = "git-pages"
   access_key = random_password.git_pages_admin_id.result
   secret_key = random_password.git_pages_sa_password.result
 }
 
-resource "rustfs_bucket_policy" "git_pages_policy" {
-  bucket = rustfs_bucket.git_pages.name
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": ["*"],
-      "Resource": ["arn:aws:s3:::${rustfs_bucket.git_pages.bucket}/*"],
-      "Condition": {
-        "StringEquals": {
-          "s3:username": ["${rustfs_serviceaccount.git_pages.access_key}"]
-        }
+resource "rustfs_policy" "git_pages_policy" {
+  name = rustfs_bucket.git_pages.name
+  statement = {
+    Effect   = "Allow"
+    Action   = ["*"]
+    Resource = ["arn:aws:s3:::${rustfs_bucket.git_pages.name}/*"]
+    Condition = {
+      StringEquals = {
+        "s3:username" = ["${rustfs_policy.git_pages.access_key}"]
       }
     }
-  ]
-}
-EOF
+  }
 }
