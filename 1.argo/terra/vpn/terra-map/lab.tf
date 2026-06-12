369fdb168ed1eced8fc7012d009c2f87e2a9fdc0
Max Batleforc <maxleriche.60@gmail.com>
2026-06-12 22:25:33 +0200

    feat: wip

--- 1.argo/terra/vpn/terra-map/lab.tf
+++ 1.argo/terra/vpn/terra-map/lab.tf
@@ -2,3 +2,28 @@ resource "netbird_network" "lab" {
   name        = "Lab Network"
   description = "Network for the lab environment also named Weebo SI"
 }
+
+resource "netbird_group" "lab" {
+  name = "lab"
+}
+
+resource "netbird_setup_key" "lab-master" {
+  name                   = "lab-master setup key"
+  expiry_seconds         = 0 # 30 days
+  type                   = "reusable"
+  allow_extra_dns_labels = true
+  auto_groups            = [netbird_group.lab.id]
+  ephemeral              = false
+  revoked                = false
+  usage_limit            = 0
+}
+
+resource "vault_kv_secret_v2" "lab" {
+  mount = "mv"
+  name  = "reserved/lab"
+  data_json = jsonencode(
+    {
+      KUBERNETES_SETUP_KEY = netbird_setup_key.lab-master.key
+    }
+  )
+}
