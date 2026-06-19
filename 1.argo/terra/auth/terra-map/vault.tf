52e0692bf9fa5f3862b002b1c4157e1500b5a6c9
Max Batleforc <maxleriche.60@gmail.com>
2026-06-19 21:21:09 +0200

    feat: wip

--- 1.argo/terra/auth/terra-map/vault.tf
+++ 1.argo/terra/auth/terra-map/vault.tf
@@ -102,6 +102,7 @@ resource "vault_jwt_auth_backend_role" "vault_authentik_weebo_admin" {
     "https://vault.weebo.poc/ui/vault/auth/authentik/oidc/callback",
     "https://vault.weebo.poc/authentik/callback",
     "http://localhost:8250/authentik/callback",
+    "http://localhost:8250/oidc/callback"
   ]
   groups_claim = "groups"
   oidc_scopes  = ["openid", "profile", "email"]
