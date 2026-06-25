a19c2e074074b4c4800746f91b8431a7deb03077
Max Batleforc <maxleriche.60@gmail.com>
2026-06-25 22:52:06 +0200

    feat: wip

--- 1.argo/terra/vpn/terra-map/lab.tf
+++ 1.argo/terra/vpn/terra-map/lab.tf
@@ -1,7 +1,7 @@
-resource "netbird_network" "lab" {
-  name        = "Lab Network"
-  description = "Network for the lab environment also named Weebo SI"
-}
+# resource "netbird_network" "lab" {
+#   name        = "Lab Network"
+#   description = "Network for the lab environment also named Weebo SI"
+# }
 
 locals {
   assign_group = [netbird_group.lab.id]
@@ -11,11 +11,11 @@ resource "netbird_group" "lab" {
   name = "lab"
 }
 
-resource "netbird_network_router" "lab" {
-  network_id  = netbird_network.lab.id
-  peer_groups = [netbird_group.lab.id]
-  enabled     = true
-}
+# resource "netbird_network_router" "lab" {
+#   network_id  = netbird_network.lab.id
+#   peer_groups = [netbird_group.lab.id]
+#   enabled     = true
+# }
 
 resource "netbird_setup_key" "lab-master" {
   name                   = "lab-master setup key"
@@ -54,59 +54,59 @@ resource "netbird_policy" "lab" {
 }
 
 
-resource "netbird_network_resource" "lab-pod-cidr-v4" {
-  network_id  = netbird_network.lab.id
-  name        = "Lab POD IPV4 CIDR"
-  description = "Lab IPV4 CIDR"
-  address     = "10.245.0.0/16"
-  groups      = local.assign_group
-  enabled     = true
-}
+# resource "netbird_network_resource" "lab-pod-cidr-v4" {
+#   network_id  = netbird_network.lab.id
+#   name        = "Lab POD IPV4 CIDR"
+#   description = "Lab IPV4 CIDR"
+#   address     = "10.245.0.0/16"
+#   groups      = local.assign_group
+#   enabled     = true
+# }
 
-resource "netbird_network_resource" "lab-pod-cidr-v6" {
-  network_id  = netbird_network.lab.id
-  name        = "Lab POD IPV6 CIDR"
-  description = "Lab IPV6 CIDR"
-  address     = "fd00:10:245::/56"
-  groups      = local.assign_group
-  enabled     = true
-}
+# resource "netbird_network_resource" "lab-pod-cidr-v6" {
+#   network_id  = netbird_network.lab.id
+#   name        = "Lab POD IPV6 CIDR"
+#   description = "Lab IPV6 CIDR"
+#   address     = "fd00:10:245::/56"
+#   groups      = local.assign_group
+#   enabled     = true
+# }
 
-resource "netbird_network_resource" "lab-service-cidr-v4" {
-  network_id  = netbird_network.lab.id
-  name        = "Lab Service IPV4 CIDR"
-  description = "Lab Service IPV4 CIDR"
-  address     = "10.128.0.0/12"
-  groups      = local.assign_group
-  enabled     = true
-}
+# resource "netbird_network_resource" "lab-service-cidr-v4" {
+#   network_id  = netbird_network.lab.id
+#   name        = "Lab Service IPV4 CIDR"
+#   description = "Lab Service IPV4 CIDR"
+#   address     = "10.128.0.0/12"
+#   groups      = local.assign_group
+#   enabled     = true
+# }
 
-resource "netbird_network_resource" "lab-service-cidr-v6" {
-  network_id  = netbird_network.lab.id
-  name        = "Lab Service IPV6 CIDR"
-  description = "Lab Service IPV6 CIDR"
-  address     = "fd00:10:128::/112"
-  groups      = local.assign_group
-  enabled     = true
-}
+# resource "netbird_network_resource" "lab-service-cidr-v6" {
+#   network_id  = netbird_network.lab.id
+#   name        = "Lab Service IPV6 CIDR"
+#   description = "Lab Service IPV6 CIDR"
+#   address     = "fd00:10:128::/112"
+#   groups      = local.assign_group
+#   enabled     = true
+# }
 
-resource "netbird_network_resource" "lab-wildcard-weebo-poc" {
-  network_id  = netbird_network.lab.id
-  name        = "Lab Wildcard weebo poc"
-  description = "Lab *.weebo.poc"
-  address     = "*.weebo.poc"
-  groups      = local.assign_group
-  enabled     = true
-}
+# resource "netbird_network_resource" "lab-wildcard-weebo-poc" {
+#   network_id  = netbird_network.lab.id
+#   name        = "Lab Wildcard weebo poc"
+#   description = "Lab *.weebo.poc"
+#   address     = "*.weebo.poc"
+#   groups      = local.assign_group
+#   enabled     = true
+# }
 
-resource "netbird_network_resource" "lab-weebo-poc" {
-  network_id  = netbird_network.lab.id
-  name        = "Lab weebo poc"
-  description = "Lab weebo.poc"
-  address     = "weebo.poc"
-  groups      = local.assign_group
-  enabled     = true
-}
+# resource "netbird_network_resource" "lab-weebo-poc" {
+#   network_id  = netbird_network.lab.id
+#   name        = "Lab weebo poc"
+#   description = "Lab weebo.poc"
+#   address     = "weebo.poc"
+#   groups      = local.assign_group
+#   enabled     = true
+# }
 
 resource "netbird_route" "lab-dns" {
   network_id = "lab-network"
